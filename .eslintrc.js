b776df3d279abc18c3779dfdd531578c1e6bc5ec
Mustafa <mostafa.dash34@gmail.com>
2023-11-10 08:57:32 +0200

    Build Structure of Application

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+/* eslint-disable no-undef */
+module.exports =
+{
+  'env': {
+    'browser': true,
+    'es2021': true
+  },
+  'extends': [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier'
+  ],
+  'overrides': [
+  ],
+  'parser': '@typescript-eslint/parser',
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module',
+  },
+  'plugins': [
+    '@typescript-eslint', 'prettier'
+  ],
+  'rules': {
+    'prettier/prettier': 0,
+    'no-console': 0,
+    'no-var': 'error',
+    'prefer-const': 'error',
+  }
+};
